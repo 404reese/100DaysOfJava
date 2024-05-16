94bfffab8441239771b203b59c90df9fce14448c
404reese <135740066+404reese@users.noreply.github.com>
2024-05-16 12:21:00 +0530

    Add files via upload
    day8 code file

---
+++ Day8_LeetCode977.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                result[i] = leftSquare;
+                left++;
+            } else {
+                result[i] = rightSquare;
+                right--;
+            }
+        }
+        return result;
+    }
+}
