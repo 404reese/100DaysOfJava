0abafa163e977540d85bda6376aa22f9492d8d4e
404reese <135740066+404reese@users.noreply.github.com>
2024-06-17 17:40:55 +0530

    Add files via upload

---
+++ Day39_LeetCode881.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int start = 0, end = people.length - 1, boats = 0;
+        while (start <= end) {
+            if (people[start] + people[end] <= limit) {
+                start++;
+            }
+            end--;
+            boats++;
+        }
+        return boats;
+    }
+}
