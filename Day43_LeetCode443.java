97bf1bb8be210feba492a8068e87038cc3abc527
404reese <135740066+404reese@users.noreply.github.com>
2024-06-21 22:43:03 +0530

    Add files via upload

---
+++ Day43_LeetCode443.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+        int indexAns = 0;
+        int index = 0;
+        
+        while (index < chars.length) {
+            char currentChar = chars[index];
+            int count = 0;
+            
+            while (index < chars.length && chars[index] == currentChar) {
+                index++;
+                count++;
+            }
+            
+            chars[indexAns++] = currentChar;
+            
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[indexAns++] = c;
+                }
+            }
+        }
+        
+        return indexAns;
+    }
+}
