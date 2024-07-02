4d001e331619bf542e6e523f070958f0a7f376a4
Riddhesh <135740066+404reese@users.noreply.github.com>
2024-07-02 09:41:45 +0530

    Add files via upload

---
+++ Day54_LeetCode74.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0 ;
+        int col = matrix[0].length -1 ;
+
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col] == target){
+                return true ;
+            }else if ( matrix[row][col]>target){
+                col--;
+            }else{
+                row++;
+            }
+        }
+
+        return false;
+    }
+}
