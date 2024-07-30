0d20248a2c1ee4efdab810b83d0c006edfa9c45f
Riddhesh <135740066+404reese@users.noreply.github.com>
2024-07-30 20:01:47 +0530

    Create Day76_LeetCode203.java

---
+++ Day76_LeetCode203.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return null;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head ;
+        ListNode curr = dummy ;
+
+        while(curr.next!=null){
+
+            if(curr.next.val==val){
+                curr.next = curr.next.next;
+            }else{
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
